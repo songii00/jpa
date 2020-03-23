2ea02bef8321beb25d2a2a7d41b1c6ee456eb920
songii00 <wks4j1004@gmail.com>
2020-03-23 16:43:45 +0900

    챕터3 영속성 관리 - 내부 동작 방식

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -25,6 +25,9 @@ public class JpaMain {
             //createMember(entityManager);
             //updateMember(entityManager);
             findMember(entityManager);
+            //persistcontext(entityManager);
+           // modify(entityManager);
+            //flush(entityManager);
             transaction.commit();
 
         }catch (Exception e){
@@ -75,6 +78,90 @@ public class JpaMain {
             System.out.println(member.getId() + " " + member.getName());
         }
 
+    }
+
+    /**
+     * 영속 / 비영속
+     * @param entityManager
+     */
+    private static void persistcontext(EntityManager entityManager){
+        // 비영속
+        Member member = new Member();
+        member.setId(101L);
+        member.setName("HelloJPA");
+
+        // 영속
+        System.out.println("BEFORE");
+        entityManager.persist(member);
+        System.out.println("AFTER");
+
+        Member findMember = entityManager.find(Member.class, 101L);
+        System.out.println("findMember id = " + findMember.getId());
+        System.out.println("findMember name = " + findMember.getName());
+
+        // 준영속
+        //entityManager.detach(member);
+        // 각채를 삭제한 상태
+        //entityManager.refresh(member);
+    }
+
+    /**
+     * 변경 감지
+     * @param entityManager
+     */
+    public static void modify(EntityManager entityManager){
+
+        Member member = new Member();
+        member.setId(150L);
+        member.setName("HelloJPA");
+
+
+        Member findMember = entityManager.find(Member.class, 150L);
+        member.setName("ZZZ");
+
+        // persis 호출 해야 되는것이 아닌가?
+        // 자바 컬렉션을 다루는 것처럼 데이터를 다루겠다.
+        //entityManager.persist(findMember);
+
+
+    }
+
+    /**
+     * 플러쉬
+     * @param entityManager
+     */
+    public static void flush(EntityManager entityManager){
+
+        Member member = new Member();
+        member.setId(150L);
+        member.setName("HelloJPA");
+        entityManager.persist(member);
+        // db 로 영속성 컨텍스트에 저장된게 날라감
+        // insert 쿼리 즉시 나감
+        // 아직 commit 은 안됨
+        // 1차 캐시 그대로 유지
+        // 영속성 컨텍스 > 쓰기저장 sql 저장소 > 여기에 있는 것만 db에 반영
+        // 쿼리 실행 시에 무조건 플러시 로 날리고 쿼리 실행함
+        entityManager.flush();
+
+    }
+
+    /**
+     * 영속 / 준영속
+     * @param entityManager
+     */
+    private static void persistcontext_2(EntityManager entityManager){
+
+        // 영속 상태
+        Member findMember = entityManager.find(Member.class, 150L);
+        findMember.setName("ZZZ");
+
+        // 준영속
+        // 영속성 컨텍스트에서 관리하지 말라고 끄집어냄
+        // 특정 엔티티만
+        entityManager.detach(findMember);
+        // 영속성 컨텍스트 안에 내용을 다 지움
+        entityManager.clear();
     }
 
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -14,6 +14,15 @@ public class Member {
     //@Column(name="username")
     private String name;
 
+    // jpa 는 기본 생성자 필요
+    public Member(){
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
